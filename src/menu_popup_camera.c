c7579c2507f67b471effba5ce9f58bb9412a0475
Cris <cristofer.cruz@gmail.com>
2026-08-19 12:47:00 -0500

    add halftone and crosshatch dither patterns

--- src/menu_popup_camera.c
+++ src/menu_popup_camera.c
@@ -447,7 +447,9 @@ const spinedit_value_names_t DitherSpinEditNames[N_DITHER_TYPES] = {
     { .value = dither_type_Fuzz,        .name = " Fuzz\t\t"  },
     { .value = dither_type_Vertical,    .name = " Vert.\t\t" },
     { .value = dither_type_Horizonral,  .name = " Horiz.\t\t"},
-    { .value = dither_type_Mix,         .name = " Mix\t\t"   }
+    { .value = dither_type_Mix,         .name = " Mix\t\t"   },
+    { .value = dither_type_Halftone,    .name = " Halftone\t"},
+    { .value = dither_type_Crosshatch,  .name = " Cross\t\t" }
 };
 const spinedit_params_t DitherSpinEditParams = {
     .caption = "Dithering:",
@@ -467,7 +469,9 @@ const uint8_t * const DitherNames[N_DITHER_TYPES] = {
     [dither_type_Fuzz]       = "Fuzz",
     [dither_type_Vertical]   = "Vert.",
     [dither_type_Horizonral] = "Horiz.",
-    [dither_type_Mix]        = "Mix"
+    [dither_type_Mix]        = "Mix",
+    [dither_type_Halftone]   = "Halftone",
+    [dither_type_Crosshatch] = "Cross"
 };
 
 
@@ -720,7 +724,9 @@ uint8_t * onCameraPopupMenuItemPaint(const struct menu_t * menu, const struct me
         [dither_type_Fuzz]              = "[Fuzz]",
         [dither_type_Vertical]          = "[Vert.]",
         [dither_type_Horizonral]        = "[Horiz.]",
-        [dither_type_Mix]               = "[Mix]"
+        [dither_type_Mix]               = "[Mix]",
+        [dither_type_Halftone]          = "[Halftone]",
+        [dither_type_Crosshatch]        = "[Cross]"
     };
 
     switch ((camera_popup_menu_e)self->id) {
